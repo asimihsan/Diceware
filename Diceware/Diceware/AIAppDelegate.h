7885f30e42f068f72ec07b937b66fa1e5c88b1f8
Asim Ihsan <asim.ihsan@gmail.com>
2011-10-28 23:14:32 +0100

    Initial commit.

---
+++ Diceware/Diceware/AIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AIAppDelegate.h
+//  Diceware
+//
+//  Created by Asim Ihsan on 28/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Diceware/Diceware/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Diceware
+//
+//  Created by Asim Ihsan on 28/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AIAppDelegate class]));
+    }
+}
